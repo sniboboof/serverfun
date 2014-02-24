ee8d1e9458fd2fd1375716ab2378787f9407f7c1
Jack Markley <jack.markley@gmail.com>
2014-02-24 03:30:35 +0100

    400 error

--- server.py
+++ server.py
@@ -1,9 +1,28 @@
 import socket
+import os
 
 #give it the request string
 #it will parse it and return the response
-def getResponse(request_string):
-    return "but why"
+def getResponse(requeststring):
+    responsestring = "HTTP/1.1"
+    
+    requesttokens = requeststring.split('\n', 1)[0].split(None, 3)
+    
+    if requesttokens[0] is "GET" \
+    and (requesttokens[2] is "HTTP/1.1" or requesttokens[2] is "HTTP/1.0"):
+        responsestring += get_request(requesttokens[1])
+    else:
+        responsestring += bad_request()
+    
+    responsestring += "\r\n"
+    return responsestring
+
+def get_request(filepath):
+    message = ""
+    return message
+
+def bad_request():
+    return " 400 Bad Request"
 
 def simple_server_loop(portnum=17745):
     httpserv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
