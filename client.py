af1498c6583b313f5b7099f1758c8ff107d6847d
Jack Markley <jack.markley@gmail.com>
2014-02-24 01:03:56 +0100

    sending and listening

--- client.py
+++ client.py
@@ -4,8 +4,10 @@ import socket
 #mostly taken from socket documentation and cewing's walkthru
 
 def msgsend(mess, portnum=17745):
+    
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect( ("", portnum) )
+    
     client.sendall(mess)
     client.shutdown(socket.SHUT_WR)
     data = ""
@@ -13,12 +15,9 @@ def msgsend(mess, portnum=17745):
     while mybuff:
         data += mybuff
         mybuff = client.recv(1024)
+    
     client.close()
     return data
 
 if __name__ == '__main__':
-    if len(sys.argv) >= 2:
-        msg = sys.argv[1]
-    else:
-        msg = input("input request:\n")
-    print "Response:\n" + msgsend(msg)
+    print(msgsend(sys.argv[1]))
--- server.py
+++ server.py
@@ -1,30 +1,27 @@
 import socket
 
-def open_local_port(whichport):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("", whichport))
-    server.listen(1)
-    return server
-
 #give it the request string
 #it will parse it and return the response
 def getResponse(request_string):
     return "but why"
 
 def simple_server_loop(portnum=17745):
-    httpserv = open_local_port(portnum)
-    conn, addr = httpserv.accept()
-    
-    data = ""
-    mybuff = conn.recv(1024)
-    while mybuff:
-        data += mybuff
+    httpserv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("", portnum))
+    server.listen(1)
+    while True:
+        conn, addr = httpserv.accept()
+        
+        data = ""
         mybuff = conn.recv(1024)
-    
-    conn.sendall(getResponse(data))
-    conn.shutdown(socket.SHUT_WR)
-    
-    conn.close()
+        while mybuff:
+            data += mybuff
+            mybuff = conn.recv(1024)
+        
+        conn.sendall(getResponse(data))
+        conn.shutdown(socket.SHUT_WR)
+        
+        conn.close()
     httpserv.close()
 
 if __name__ == "__main__":
